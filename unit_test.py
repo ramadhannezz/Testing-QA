e0e4dc5ed2ff6308a63f1d26cf73b5b814081d2e
ramadhannezz <108265300+ramadhannezz@users.noreply.github.com>
2023-11-02 01:40:56 +0700

    Add files via upload

---
+++ unit_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+def add(x, y):
+    return x + y
+
+class TestAddition(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ whitebox_test.py
@@ -0,0 +1,17 @@
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x / y
+
+import pytest
+
+def test_divide_positive_numbers():
+    result = divide(10, 2)
+    assert result == 5
+
+def test_divide_by_zero_raises_exception():
+    with pytest.raises(ValueError):
+        divide(10, 0)
+
+if __name__ == '__main__':
+    pytest.main()
